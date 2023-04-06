af0a2045309b41771303b87c31ec3f3a3458a70f
Renato Araújo <renatoassuncaodearaujo@gmail.com>
2023-04-05 21:05:18 -0300

    same

--- app/admin/group/__init__.py
+++ app/admin/group/__init__.py
@@ -1,4 +1,15 @@
 """Group blueprint"""
-from flask import Blueprint
+from flask_restx import Namespace
 
-group_bp = Blueprint("admin_group", __name__)
+from app.admin.group.swagger.models import (
+    create_group_model,
+    group_model,
+    update_group_model,
+)
+
+group_api = Namespace("admin_group", description="User groups")
+
+# models
+group_api.models["group"] = group_model
+group_api.models["create_group"] = create_group_model
+group_api.models["update_group"] = update_group_model
