7e53dade923c5cc3b3354b9fbba36517d2d71c83
Renato Araújo <renatoassuncaodearaujo@gmail.com>
2023-04-05 22:23:26 -0300

    migrate services extensions to restx

--- app/services/extensions.py
+++ app/services/extensions.py
@@ -6,30 +6,34 @@ from flask_caching import Cache
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+
+from config import Config
 
 cors = CORS()
-cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
+cache = Cache()
 jwt = JWTManager()
+migrate = Migrate()
 ma = Marshmallow()
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    aws_access_key_id=Config.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=Config.AWS_SECRET_ACCESS_KEY,
 )
 
 # aws simple notification service
 sns = boto3.client(
     "sns",
-    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    aws_access_key_id=Config.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=Config.AWS_SECRET_ACCESS_KEY,
     region_name="us-east-1",
 )
 
 # aws simple email service
 ses = boto3.client(
     "ses",
-    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    aws_access_key_id=Config.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=Config.AWS_SECRET_ACCESS_KEY,
     region_name="us-east-1",
 )
